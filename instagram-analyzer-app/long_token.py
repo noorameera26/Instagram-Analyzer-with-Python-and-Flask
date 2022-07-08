d6356447c5c2da7535b2fbceb7e28db3db64ae31
= <=>
2022-07-09 00:51:14 +0800

    update token files

--- instagram-analyzer-app/long_token.py
+++ instagram-analyzer-app/long_token.py
@@ -11,3 +11,22 @@ def getLongLivedAccessToken(params):
     Returns:
         Object: data from the endpoint
     """
+
+    endpointsParams = dict()
+    endpointsParams['grant_type'] = 'fb_exchange_token'
+    endpointsParams['client_id'] = params['client_id']
+    endpointsParams['client_secret'] = params['client_secret']
+    endpointsParams['fb_exchange_token'] = params['access_token']
+    
+    url = params['endpoint_base'] + 'oauth/access_token'
+
+    return makeApiCall(url, endpointsParams, params['debug'])
+
+params = getCreds()
+params['debug'] = 'yes'
+response = getLongLivedAccessToken(params)
+
+print("\n ---- ACCESS TOKEN INFO --- \n")
+print("Long Lived Access Token:")
+print(response['json_data']['access_token'])
+    
